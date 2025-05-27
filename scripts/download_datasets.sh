52cae3665d19665ae64deaa84b509996147715c2
Notherthing <1040986768@qq.com>
2025-05-27 22:37:20 +0800

    5.27

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -1,4 +1,5 @@
 export HF_ENDPOINT=http://hf-mirror.com;
 huggingface-cli download Qwen/Qwen-tokenizer --local-dir ./input/Qwen-tokenizer;
-huggingface-cli download --repo-type dataset --resume-download stanfordnlp/imdb --local-dir ./input/sft-data ;
+huggingface-cli download --repo-type dataset --resume-download Skywork/SkyPile-150B --local-dir ./input/SkyPile-150B;
+huggingface-cli download --repo-type dataset --resume-download stanfordnlp/imdb --local-dir ./input/;
 huggingface-cli download --repo-type dataset --resume-download trl-lib/ultrafeedback_binarized --local-dir ./input/dpo-data 
